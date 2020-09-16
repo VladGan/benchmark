bd798cb822bd1c3abed9f3aae2ac2206397f9dbc
James Fletcher <38212943+jmsfltchr@users.noreply.github.com>
2020-09-16 12:09:58 +0100

    grakn-2.0 50 iteations

--- SimulationRunner.java
+++ SimulationRunner.java
@@ -19,6 +19,8 @@ package grakn.simulation;
 
 import grabl.tracing.client.GrablTracing;
 import grabl.tracing.client.GrablTracingThreadStatic;
+import grakn.client.Grakn;
+import grakn.client.rpc.GraknClient;
 import grakn.simulation.config.Config;
 import grakn.simulation.config.ConfigLoader;
 import grakn.simulation.common.Simulation;
@@ -109,7 +111,7 @@ public class SimulationRunner {
                 Simulation<?, ?> simulation;
                 switch (dbName) {
                     case "grakn":
-                        defaultUri = "localhost:48555";
+                        defaultUri = GraknClient.DEFAULT_URI;
                         if (hostUri == null) hostUri = defaultUri;
 
                         simulation = new GraknSimulation(
